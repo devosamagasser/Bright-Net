5faf0f8e4930b86f07492c2dd54c4a060d76a69a
devosamagasser <142623370+devosamagasser@users.noreply.github.com>
2025-10-29 12:49:50 +0300

    Refactor supplier engagement service to leverage Eloquent

--- app/Modules/SupplierEngagements/Presentation/Resources/SupplierSubcategoryResource.php
+++ app/Modules/SupplierEngagements/Presentation/Resources/SupplierSubcategoryResource.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * @property array{
+ *     supplier_department: array{id:int, name:string|null},
  *     id:int,
- *     name:string|null
+ *     name:string|null,
+ *     families?: array<int, array{id:int, name:string|null}>
  * } $resource
  */
 class SupplierSubcategoryResource extends JsonResource
@@ -18,9 +20,26 @@ class SupplierSubcategoryResource extends JsonResource
      */
     public function toArray($request): array
     {
+        $supplierDepartment = $this->resource['supplier_department'] ?? [];
+        $families = $this->resource['families'] ?? [];
+
+        if (! is_array($families)) {
+            $families = [];
+        }
+
         return [
+            'supplier_department' => [
+                'id' => (int) ($supplierDepartment['id'] ?? 0),
+                'name' => $supplierDepartment['name'] ?? null,
+            ],
             'id' => (int) ($this->resource['id'] ?? 0),
             'name' => $this->resource['name'] ?? null,
+            'families' => array_map(static function (array $family): array {
+                return [
+                    'id' => (int) ($family['id'] ?? 0),
+                    'name' => $family['name'] ?? null,
+                ];
+            }, $families),
         ];
     }
 }
