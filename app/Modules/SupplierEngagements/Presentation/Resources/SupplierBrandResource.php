fe82048f88fcb29a0a7943cb12a974d5968986a7
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2025-11-05 11:23:06 +0200

    brand origin

--- app/Modules/SupplierEngagements/Presentation/Resources/SupplierBrandResource.php
+++ app/Modules/SupplierEngagements/Presentation/Resources/SupplierBrandResource.php
@@ -22,6 +22,7 @@ class SupplierBrandResource extends JsonResource
     {
         return [
             'supplier_brand_id' => (int) ($this->resource['supplier_brand_id'] ?? 0),
+            'id' => (int) ($this->resource['id'] ?? 0),
             'name' => $this->resource['name'] ?? null,
             'region' => $this->resource['region'] ?? null,
             'logo' => $this->resource['logo'] ?? null,
