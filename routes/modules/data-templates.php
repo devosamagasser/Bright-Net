d4d01180711dc17608e89cc4326429aaa8934c19
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2025-12-02 15:54:23 +0200

    random filterable fileds generator in data template

--- routes/modules/data-templates.php
+++ routes/modules/data-templates.php
@@ -13,13 +13,25 @@ Route::prefix('data-templates')
         Route::get('/{dataTemplate}', [DataTemplateController::class, 'show']);
         Route::put('/{dataTemplate}', [DataTemplateController::class, 'update']);
         Route::delete('/{dataTemplate}', [DataTemplateController::class, 'destroy']);
-        Route::get('random-filterable/{dataTemplate}', function ($dataTemplate) {
+        Route::get('random-filterable/{dataTemplate}/true', function ($dataTemplate) {
             DataField::query()
                 ->where('data_template_id', $dataTemplate)
                 ->inRandomOrder()
+                ->limit(5)
                 ->get()
                 ->each(function (DataField $field) {
                     $field->is_filterable = true;
+                    $field->save();
+                });
+        })->whereNumber('dataTemplate');
+        Route::get('random-filterable/{dataTemplate}/false', function ($dataTemplate) {
+            DataField::query()
+                ->where('data_template_id', $dataTemplate)
+                ->inRandomOrder()
+                ->limit(8)
+                ->get()
+                ->each(function (DataField $field) {
+                    $field->is_filterable = false;
                     $field->save();
                 });
         })->whereNumber('dataTemplate');
